729aae89a3abf7493c706f4d2159a4597f3ef3ce
KasatkinVl <KasatkinVl@ya.ru>
2020-05-14 14:21:31 +0300

    for update

--- work_area.py
+++ work_area.py
@@ -1,38 +1,20 @@
-n = int(input())
-a = [[0 for j in range(n)] for i in range(n)]
-x = n
-dx, dy = 0,0
+s = input().lower().split()
+
+modify = s + [' ']
+i = 0
+j = len(modify) - 1
 count = 1
+res = {}
+while i < j:
+    if modify[i + 1] == modify[i]:
+        count += 1
+    else:
+        res += modify[i] + str(count)
+        count = 1
+    i += 1
+print(res)
 
-if n == 1:
-    print(1)
-else:
 
-    while x != 0:
+#a aa abC aa ac abc bcd a
 
-        for i in range(1):
-            for j in range(n-1):
-                a[i+dy][j] = count
-                count += 1
-
-        # for i in range(dy,n):
-        #     for j in range(1):
-        #         a[i][j-dx] = n + i
-        #
-        # for i in range(n-1,n):
-        #     for j in range(n-2,-1,-1):
-        #         a[i][j] = 3*n-2-j
-        #
-        # for i in range(n-2,0,-1):
-        #     for j in range(1):
-        #         a[i][j] = 3*n-2+(n-i-1)
-
-        dx += 1
-        dy += 1
-        x -= n//2
-
-    for i in range(n):
-        print()
-        for j in range(n):
-            print(a[i][j], end=' ')
 
