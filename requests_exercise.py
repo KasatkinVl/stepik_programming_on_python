8fc1e9acae60772dc623a821e7f0dbed5f6f0970
KasatkinVl <KasatkinVl@ya.ru>
2020-05-16 00:07:11 +0300

    for update

--- requests_exercise.py
+++ requests_exercise.py
@@ -3,9 +3,10 @@ import requests
 with open('C:\\Users\\User\\Downloads\\file.txt', encoding='utf-8') as text:
     url = text.readline().strip()
 
-r = requests.get(url)
+r = requests_exercise.get(url)
 text = r.text.splitlines()
 
 
 print(len(text))
 
+#НЕЛЬЗЯ НАЗЫВАТЬ ФАЙЛЫ ИМЕНАМИ БИБЛИОТЕК
